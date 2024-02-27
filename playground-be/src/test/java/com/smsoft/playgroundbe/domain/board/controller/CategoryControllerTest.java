76c6b0b90737f8c1effb0a089fc841060a340a37
kimsumin <tnals1564@naver.com>
2024-02-27 17:56:26 +0900

    category service, controller testcode complete

---
+++ playground-be/src/test/java/com/smsoft/playgroundbe/domain/board/controller/CategoryControllerTest.java
@@ -0,0 +1,110 @@
+package com.smsoft.playgroundbe.domain.board.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smsoft.playgroundbe.domain.board.dto.CategoryDTO;
+import com.smsoft.playgroundbe.domain.board.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.swing.plaf.PanelUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CategoryController.class)
+@ActiveProfiles("test")
+class CategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setup(WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    @DisplayName("카테고리 생성 API 테스트")
+    public void createCategory() throws Exception {
+        CategoryDTO.Request requestDTO = new CategoryDTO.Request();
+        requestDTO.setName("기술");
+        CategoryDTO.Response responseDTO = new CategoryDTO.Response(1L, "기술");
+
+        given(categoryService.createCategory(Mockito.any(CategoryDTO.Request.class))).willReturn(responseDTO);
+
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is("기술")));
+    }
+
+    @Test
+    @DisplayName("카테고리 업데이트 API 테스트")
+    public void updateCategory() throws Exception {
+        Long categoryId = 1L;
+        CategoryDTO.Request requestDTO = new CategoryDTO.Request();
+        requestDTO.setName("신기술");
+        CategoryDTO.Response responseDTO = new CategoryDTO.Response(categoryId, "신기술");
+
+        given(categoryService.updateCategory(Mockito.eq(categoryId), Mockito.any(CategoryDTO.Request.class))).willReturn(responseDTO);
+
+        mockMvc.perform(put("/categories/{id}", categoryId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("신기술")));
+    }
+
+    @Test
+    @DisplayName("카테고리 삭제 API 테스트")
+    public void deleteCategory() throws Exception {
+        Long categoryId = 1L;
+
+        Mockito.doNothing().when(categoryService).deleteCategory(categoryId);
+
+        mockMvc.perform(delete("/categories/{id}", categoryId))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("카테고리 조회 API 테스트")
+    public void getCategoriesTest() throws Exception {
+        List<CategoryDTO.Response> categories = Arrays.asList(new CategoryDTO.Response(1L, "기술"), new CategoryDTO.Response(2L, "스포츠"));
+
+        given(categoryService.getAllCategories()).willReturn(categories);
+
+        mockMvc.perform(get("/categories")
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("기술")))
+                .andExpect(jsonPath("$[1].name", is("스포츠")));
+    }
+
+}
--- playground-be/src/test/java/com/smsoft/playgroundbe/domain/board/service/CategoryServiceTest.java
+++ playground-be/src/test/java/com/smsoft/playgroundbe/domain/board/service/CategoryServiceTest.java
@@ -1,18 +1,31 @@
 package com.smsoft.playgroundbe.domain.board.service;
 
+import com.smsoft.playgroundbe.domain.board.dto.CategoryDTO;
+import com.smsoft.playgroundbe.domain.board.entity.Category;
 import com.smsoft.playgroundbe.domain.board.repository.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceUnit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@ActiveProfiles("test")
+@SpringBootTest
 class CategoryServiceTest {
 
     @MockBean
@@ -23,7 +36,99 @@ class CategoryServiceTest {
 
     @Test
     @DisplayName("카테고리 생성 - 성공 테스트")
-    public void createCategory_Sucess() {
+    public void createCategorySuccess() {
+        // Given
+        CategoryDTO.Request categoryDTO = new CategoryDTO.Request();
+        categoryDTO.setName("기술");
+        Category mockCategory = new Category(1L, "기술", new ArrayList<>());
+        Mockito.when(categoryRepository.save(any(Category.class))).thenReturn(mockCategory);
 
+        // When
+        CategoryDTO.Response createdCategory = categoryService.createCategory(categoryDTO);
+
+        // Then
+        assertNotNull(createdCategory);
+        assertEquals("기술", createdCategory.getName());
+    }
+
+    @Test
+    @DisplayName("카테고리 업데이트 - 성공 테스트")
+    public void updateCategorySuccess() {
+        // Given
+        Long categoryId = 1L;
+        CategoryDTO.Request categoryDTO = new CategoryDTO.Request();
+        categoryDTO.setName("스포츠");
+        Category existingCategory = new Category(1L, "기술", new ArrayList<>());
+        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
+        Mockito.when(categoryRepository.save(any(Category.class))).thenReturn(existingCategory);
+
+        // When
+        CategoryDTO.Response updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+
+        // Then
+        assertEquals("스포츠", updatedCategory.getName());
+    }
+
+    @Test
+    @DisplayName("카테고리 업데이트 - 실패 테스트(Not Found)")
+    public void updateCategoryNotFound() {
+        // Given
+        Long categoryId = 1L;
+        CategoryDTO.Request categoryDTO = new CategoryDTO.Request();
+        categoryDTO.setName("스포츠");
+        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
+
+        // When & Then
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            categoryService.updateCategory(categoryId, categoryDTO);
+        });
+
+        assertEquals(exception.getMessage(), "Category not found with id: " + categoryId);
+    }
+
+    @Test
+    @DisplayName("카테고리 삭제 - 성공 테스트")
+    public void deleteCategorySuccess() {
+        // Given
+        Long categoryId = 1L;
+        Category existingCategory = new Category(1L, "기술", new ArrayList<>());
+        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
+
+        // When
+        categoryService.deleteCategory(categoryId);
+
+        // Then
+        Mockito.verify(categoryRepository, Mockito.times(1)).delete(existingCategory);
+    }
+
+    @Test
+    @DisplayName("카테고리 삭제 - 실패 테스트(Not Found)")
+    public void deleteCategoryNotFound() {
+        // Given
+        Long categoryId = 1L;
+        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
+
+        // When & Then
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            categoryService.deleteCategory(categoryId);
+        });
+
+        assertEquals("Category not found with id: " + categoryId, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("카테고리 전체조회 - 성공 테스트")
+    public void getCategoriesSuccess() {
+        // Given
+        List<Category> categories = Arrays.asList(new Category(1L, "기술", new ArrayList<>()), new Category(2L, "과학", new ArrayList<>()));
+        Mockito.when(categoryRepository.findAll()).thenReturn(categories);
+
+        // When
+        List<CategoryDTO.Response> allCategories = categoryService.getAllCategories();
+
+        // Then
+        assertEquals(2, allCategories.size());
+        assertEquals("기술", allCategories.get(0).getName());
+        assertEquals("과학", allCategories.get(1).getName());
     }
 }
